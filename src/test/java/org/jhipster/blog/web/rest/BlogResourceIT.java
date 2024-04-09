1247326501680f5e27ff63a82f1c94061d6d6a2d
Matt Raible <matt@raibledesigns.com>
2024-04-09 14:38:05 -0400

    apply updated prettier to actual application

--- src/test/java/org/jhipster/blog/web/rest/BlogResourceIT.java
+++ src/test/java/org/jhipster/blog/web/rest/BlogResourceIT.java
@@ -11,7 +11,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityManager;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
